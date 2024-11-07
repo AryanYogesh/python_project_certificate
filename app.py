aa06b4c2578880cc2d56d7565006c0d6df5ac53d
AryanYogesh <aryanyogesh1947@gmail.com>
2024-11-07 10:30:05 +0530

    third commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Request,render_template
+from flask import Flask, request, render_template
 
 from certificate.certificates import cerf
 
@@ -8,19 +8,27 @@ app=Flask(__name__)
 def index():
     return render_template('certificate_form.html')
 
-@app.route("/certificate_form.html",methods='POST')
+@app.route("/generate",methods=['POST'])
 def generate():
-    name = Request.form.get('name')
-    course = Request.form.get('course')
-    all_classes = Request.form.get('all_classes')
-    all_assignments = Request.form.get('all_assignments')
+    name = request.form.get('name')
+    course = request.form.get('course')
+    all_classes = request.form.get('all_classes')
+    all_assignments = request.form.get('all_assignments')
 
     certificate=cerf(name,course,all_classes,all_assignments)
     dicts=certificate.generate_certificate()
     name=dicts['name']
     course=dicts['course']
-    if all_classes=='yes' and all_assignments=='yes':
+    if all_classes=='Yes' and all_assignments=='Yes':
+        print("all_classes:", all_classes)
+        print("all_assignments:", all_assignments)
+
         return render_template('generate.html',name=name,course=course)
+    
     
     else:
         return render_template('not_completed.html')
+    
+
+if __name__=='__main__':
+    app.run(debug=True)
